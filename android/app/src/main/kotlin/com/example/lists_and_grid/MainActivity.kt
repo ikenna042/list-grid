7c7d21636e2808c2af90530a9298f9c805c344f1
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-08-24 09:52:31 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/example/lists_and_grid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lists_and_grid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Colors.white70,
+          appBar: AppBar(bottom: TabBar(tabs: [Text('List'), Text('Grid')])),
+          body: TabBarView(
+              children: [Text('list content'), Text('grid content')]),
+        ),
+      ),
+    );
+  }
+}
