ae890b7a7f7a176e395ae3cfdd3549885786267a
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-08-24 12:36:39 +0100

    added dynamic list and grid

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _users = List.filled(20, 'Chidera');
+  final _lovers = List.filled(20, 'Ikenna');
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,10 +20,30 @@ class _MyAppState extends State<MyApp> {
         child: Scaffold(
           backgroundColor: Colors.white70,
           appBar: AppBar(bottom: TabBar(tabs: [Text('List'), Text('Grid')])),
-          body: TabBarView(
-              children: [Text('list content'), Text('grid content')]),
+          body: TabBarView(children: [_contentListView(), _contentGridView()]),
         ),
       ),
+    );
+  }
+
+  Widget _contentListView() {
+    return ListView.builder(
+      itemCount: _users.length,
+      itemBuilder: (context, index) => Card(
+        child: ListTile(
+          title: Text(_users[index]),
+        ),
+      ),
+    );
+  }
+
+  Widget _contentGridView() {
+    return GridView.builder(
+      itemCount: _lovers.length,
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (context, index) =>
+          Card(child: GridTile(child: Center(child: Text(_lovers[index])))),
     );
   }
 }
